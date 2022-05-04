d114aafdf82d6be75e54e7a6cbd57c802d7772a9
Artsiom Meka <42277257+Kaifaat@users.noreply.github.com>
2022-05-04 23:09:08 +0300

    deleted comments

--- Lesson-7/homeWork5.js
+++ Lesson-7/homeWork5.js
@@ -1,4 +1,4 @@
-/*function Cat(name) {
+function Cat(name) {
     this.name = name;
     this.feed = function () {
         console.log('Насыпаем в миску ' + formatFoodAmount() + ' корма');
@@ -12,40 +12,17 @@
 var myCat = new Cat('Steve');
 console.log(myCat.feed());
 
-*/
-
-/*function Cat(name) {
-    this.name = name;
-    this.feed = function () {
-        console.log('Насыпаем в миску ' + this.dailyNorm() + 'гр корма');
-    };
-    var foodAmount = 0;
-    this.dailyNorm = function (amount) {
-        if (amount < 50 || amount > 100) {
-            throw new Error('Не пойму, вы хотите перекормить кота, или оставить голодным?')
-        } else if (!arguments.length) {
-            return foodAmount;
-        } else {
-            return foodAmount = amount;
-        }
-    }
-}
-
-var myCat = new Cat('Steve');
-myCat.dailyNorm(76);
-myCat.feed();*/
 
 function Cat(name) {
     this.name = name;
     this.feed = function () {
         console.log('Насыпаем в миску ' + formatFoodAmount() + ' корма');
     };
-    var foodAmount = 56;
+    var foodAmount = 56;git
     function formatFoodAmount() {
         return foodAmount + 'гр';
     };
     this.dailyNorm = function (amount) {
-
         if (!arguments.length) {
             return formatFoodAmount()
         } else if (amount < 50 || amount > 100) {
