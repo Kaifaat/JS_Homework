b61703a62ed961390613bba02e8c6a2d513c1b17
Artsiom Meka <artem.meka@gmail.com>
2022-05-05 10:53:40 +0300

    Homework 5

--- Lesson-7/homeWork5.js
+++ Lesson-7/homeWork5.js
@@ -10,33 +10,34 @@ function Cat(name) {
         return foodAmount + 'гр';
     }
 }
+
 var myCat = new Cat('Steve');
 console.log(myCat.feed());
 
-    //3
-    /*function Cat(name) {
-        this.name = name;
-        this.feed = function () {
-            return 'Насыпаем в миску ' + formatFoodAmount() + ' корма';
-        };
-        var foodAmount = 50;
+//3
+function Cat(name) {
+    this.name = name;
+    this.feed = function () {
+        return 'Насыпаем в миску ' + formatFoodAmount() + ' корма';
+    };
+    var foodAmount = 50;
 
-        function formatFoodAmount() {
-            return foodAmount + 'гр';
-        }
-
-        this.dailyNorm = function (amount) {
-            if (!arguments.length) {
-                return formatFoodAmount()
-            } else if (amount < 50 || amount > 100) {
-                throw new Error('Не пойму, вы хотите перекормить кота, или оставить голодным?')
-            } else {
-                foodAmount = amount;
-            }
-        }
+    function formatFoodAmount() {
+        return foodAmount + 'гр';
     }
 
-    var myCat = new Cat('Steve');
-    myCat.dailyNorm(75)
-    console.log(myCat.dailyNorm());
-    console.log(myCat.feed());*/
+    this.dailyNorm = function (amount) {
+        if (!arguments.length) {
+            return formatFoodAmount()
+        } else if (amount < 50 || amount > 100) {
+            throw new Error('Не пойму, вы хотите перекормить кота, или оставить голодным?')
+        } else {
+            foodAmount = amount;
+        }
+    }
+}
+
+var myCat = new Cat('Steve');
+myCat.dailyNorm(75)
+console.log(myCat.dailyNorm());
+console.log(myCat.feed());
