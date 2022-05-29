e88915ba7ffecd149458e1251f74a6fbc3dbdfb3
Artsiom Meka <42277257+Kaifaat@users.noreply.github.com>
2022-05-29 23:02:38 +0300

    homework10

---
+++ Lesson-13/Task1/homework10Task1.js
@@ -0,0 +1,45 @@
+var table = document.getElementById('table');
+var button = document.getElementById('button');
+var addTd = document.getElementsByTagName('TD');
+var input = document.createElement('input');
+input.type = 'text';
+
+var addRow = function () {
+    var addTr = document.createElement('tr');
+    var rows = document.getElementsByTagName('tr');
+    addTr.innerHTML = "<td></td><td></td><td></td>";
+
+    table.insertBefore(addTr, rows[0]);
+}
+
+table.onclick = function (event) {
+    var target = event.target;
+    if (target.tagName === 'TD' && target.id !== 'button') {
+        addInput(target)
+    }
+}
+
+function addInput(node) {
+    addTd = node;
+    var text = event.target.textContent;
+    event.target.appendChild(input).focus();
+    input.value = text;
+    addTd.setAttribute('style', 'color: white;')
+
+}
+
+table.addEventListener("focusout", function (event) {
+    addTd.innerHTML = event.target.value;
+    event.target.value = null;
+    addTd.setAttribute('style', 'color: black;')
+});
+
+function pressEnter(event) {
+    if (event.keyCode === 13) {
+        addTd.innerHTML = event.target.value;
+    }
+}
+
+table.addEventListener('keydown', pressEnter);
+button.addEventListener('click', addRow);
+
